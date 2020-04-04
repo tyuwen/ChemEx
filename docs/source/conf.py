cd2de899c346ec4352e00726e65a45c0cc108eac
Tairan Yuwen <tyuwen@purcell.chem.purdue.edu>
2020-04-04 13:23:12 +0800

    numpy doc removed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ release = version
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax', 'numpydoc', 
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax', 
 'sphinx.ext.autosummary', 'recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
